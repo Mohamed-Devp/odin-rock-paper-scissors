0e3d63f5e7f5e6931effad870abe59d65301c7aa
Mohamed Marghoub <mohamedmarghoub0726@gmail.com>
2026-06-11 11:28:59 +0100

    Rewrite logic to play the game

--- index.js
+++ index.js
@@ -1,66 +1,148 @@
-let computerScore = 0;
+const selectCards = document.querySelectorAll(".card_type_select");
+
+const srStatus = document.querySelector(".sr-status");
+
+const statusBox = document.querySelector(".status");
+const statusBoxContent = document.querySelector(".status__content");
+
+const nextRoundBtn = document.querySelector(".btn_label_next-round");
+const playAgainBtn = document.querySelector(".btn_label_play-again");
+
+const computerChoiceCard = document.querySelector(".card_label_computer-choice");
+
+let computerChoice;
+
 let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
+    computerChoiceCard.classList.remove(
+        "is-hidden", "card_choice_rock", "card_choice_paper", "card_choice_scissors"
+    );
+
     const randomNumber = Math.floor(Math.random() * 3) + 1;
 
-    if (randomNumber === 1) {
-        return "rock";
-    }
-    else if (randomNumber === 2) {
-        return "paper";
-    }
+    let choice;
 
-    return "scissors";
-}
+    switch (randomNumber) {
+        case 1:
+            computerChoiceCard.classList.add("card_choice_rock");
+            choice = "rock";
+            break;
 
-function getPlayerChoice() {
-    const validChoices = ["rock", "paper", "scissors"];
-
-    let choice = prompt("Enter your choice:").toLowerCase();
-
-    while (!validChoices.includes(choice)) {
-        choice = prompt("Please enter a valid choice (i.e., rock or paper or scissors):").toLowerCase(); 
+        case 2:
+            computerChoiceCard.classList.add("card_choice_paper");
+            choice = "paper";
+            break;
+        
+        case 3:
+            computerChoiceCard.classList.add("card_choice_scissors");
+            choice = "scissors";
     }
 
     return choice;
 }
 
-function playRound(playerChoice, computerChoice) {
-    const winCombinations = ["rock scissors", "paper rock", "scissors paper"];
-    const loseCombinations = ["rock paper", "paper scissors", "scissors rock"];
+function updateStatus(msg) {
+    statusBox.classList.remove("is-hidden");
 
-    const currentCombination = `${playerChoice} ${computerChoice}`;
+    statusBoxContent.textContent = msg;
+    srStatus.textContent = `The computer picked ${computerChoice}; ${msg}`;
+}
 
-    if (winCombinations.includes(currentCombination)) {
-        alert(`You win! ${playerChoice} beats ${computerChoice}.`);
+function playRound(playerChoice) {
+    const choices = `${playerChoice} ${computerChoice}`;
+
+    const isWin =
+        choices === "rock scissors" ||
+        choices === "paper rock" ||
+        choices === "scissors paper";
+    
+    const isLost =
+        choices === "rock paper" ||
+        choices === "paper scissors" ||
+        choices === "scissors rock";
+    
+    if (isWin) {
         playerScore += 1;
+
+        if (playerScore === 3) {
+            updateStatus("You won the game!");
+
+            nextRoundBtn.classList.add("is-hidden");
+            playAgainBtn.classList.remove("is-hidden");
+
+            playAgainBtn.focus();
+        }
+        else {
+            updateStatus("You won this round!");
+            nextRoundBtn.focus();
+        }
     }
-    else if (loseCombinations.includes(currentCombination)) {
-        alert(`You lose! ${computerChoice} beats ${playerChoice}.`);
+    else if (isLost) {
         computerScore += 1;
+
+        if (computerScore === 3) {
+            updateStatus("You lost the game!");
+
+            nextRoundBtn.classList.add("is-hidden");
+            playAgainBtn.classList.remove("is-hidden");
+
+            playAgainBtn.focus();
+        }
+        else {
+            updateStatus("You lost this round!");
+            nextRoundBtn.focus();
+        }
     }
     else {
-        alert("A draw!");
+        updateStatus("It's a tie!");
+        nextRoundBtn.focus();
     }
 }
 
-function playGame() {
-    const numberOfRounds = 5;
+function playNextRound() {
+    statusBox.classList.add("is-hidden");
+    computerChoiceCard.classList.add("is-hidden");
 
-    for (let i = 0; i < numberOfRounds; i++) {
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
+    srStatus.textContent = "";
 
-        playRound(playerSelection, computerSelection);
-    }
+    selectCards.forEach((card, index) => {
+        card.disabled = false;
+        card.classList.remove("is-hidden");
 
-    if (playerScore > computerScore) {
-        alert(`Congratulations; You won! You ${playerScore} - ${computerScore} Computer`);
-    }
-    else {
-        alert(`Better luck next time; You lost! You ${playerScore} - ${computerScore} Computer`)
-    }
+        if (index === 0) {
+            card.focus();
+        }
+    });
 }
 
-playGame();
+selectCards.forEach(card => {
+    card.addEventListener("click", () => {
+        computerChoice = getComputerChoice();
+        
+        card.disabled = true;
+
+        selectCards.forEach(card => {
+            if (!card.disabled) {
+                card.classList.add("is-hidden");
+            }
+        });
+
+        playRound(
+            card.getAttribute("data-choice")
+        );
+    });
+});
+
+nextRoundBtn.addEventListener("click", playNextRound);
+
+playAgainBtn.addEventListener("click", () => {
+    playNextRound();
+
+    playerScore = 0;
+    computerScore = 0;
+
+    nextRoundBtn.classList.remove("is-hidden");
+    playAgainBtn.classList.add("is-hidden");
+});
